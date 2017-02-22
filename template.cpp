7504669d5809503daa0426dfe8fd9dd4346b386b
Pranay Kothapalli <pranaykothapalli@gmail.com>
2017-02-22 17:24:35 +0530

    added template.cpp, renamed pattern challenge names

--- template.cpp
+++ template.cpp
@@ -12,57 +12,39 @@
 /******************************************************************************************************/
 
 /***********************************
-/* Get minimum element in an array
+/* ALGORITHM NAME (same as the readme name)
 /***********************************/
 
-#include<iostream>
+// Library imports
+#include <iostream>
 using namespace std;
 
-// Function Prototype
-int GetMinimumElementInArray(int*, int);
+//Prototypes for the algorithms
+// void function1(int);
+// int  function2(int);
+// etc.
 
 int main(){
-	int testArr[] = {10,9,5,6,7,8,2,1,3,4};
+	// Lines to handle user input go here, be descriptive...
+	// use cin, cout 
 
-	// Calculating array size to pass to the argument of the algorithm function,
-	// since you can't really find out the length of an array since you pass the pointer
-	// of the array as an argument and the size of the whole array is lost
-	// to avoid this, we calculate the length before hand and pass it as an argument.
-	int arrSize = sizeof(testArr)/sizeof(testArr[0]); 
+	// Call your algorithms here
+	// function1(integerArgument);
+	// function2(integerArgument);
 
-	cout<<GetMinimumElementInArray(testArr,arrSize); 
-
-	return 1;
+	return 0;
 }
-
 
 /**********************************************************
-/*                <printPattern_02>
+/*                <some_function_name>
 /*
-/* @description : Loops through the array and returns the minimum element
-/* @param       : Integer<input>, 'input' represents the n value in nxn grid.
-/* @return      : None, void
-/* @complexity  : 0(n)
-/* @explanation : An algorithm that iterates over the array and tracks the least element it's
-                  encountered, reassigns the new, lower element if its lower than the current lower
-                  value. The lowest value is found upon completion of iteration in the end. 
-/* @author      : Pranay Kothapalli @kotAPI
+/* @description : description here
+/* @param       : Integer<variable> (Datatype<variable_name>)
+/* @return      : datatype (int,float,double etc)
+/* @complexity  : Big O notation. (O(n),O(n^2) etc)
+/* @explanation : Detailed description about what the algorithm does
+/* @author      : <Your name> @<GitHub handle> (John Doe @johndoe)
 /**********************************************************/
-int GetMinimumElementInArray(int* array, int size){
-	int minElement=array[0]; //Initialize the minimum element it to first element of the array
-	
-	//Loop through the array
-	for(int i=0;i<size;i++){
-		// If an element is lesser than the present minElement, reassign minElement to curr element.
-		if(minElement > array[i]){ 
-			minElement = array[i];
-		}
-	}
-
-	// Return minElement on completion
-	return minElement; 
+void some_function_name(){
+	// Algorithm content goes here.
 }
-
-
-
-
