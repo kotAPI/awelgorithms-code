fdcd2c44727d8e74668287a0bb340d3262877b7b
Pranay Kothapalli <pranaykothapalli@gmail.com>
2017-02-23 10:10:33 +0530

    changed ASCII art in templates

--- Pattern Algorithms/09_PatternChallenge.cpp
+++ Pattern Algorithms/09_PatternChallenge.cpp
@@ -1,17 +1,17 @@
 
 /*******************************************************************************************************
-/*      ___                   _                      _  _    _                      
+/*      ___                   _                      _  _    _ 
 /*     / _ \                 | |                    (_)| |  | |                     
 /*    / /_\ \__      __  ___ | |  __ _   ___   _ __  _ | |_ | |__   _ __ ___   ___  
 /*    |  _  |\ \ /\ / / / _ \| | / _` | / _ \ | '__|| || __|| '_ \ | '_ ` _ \ / __| 
 /*    | | | | \ V  V / |  __/| || (_| || (_) || |   | || |_ | | | || | | | | |\__ \ 
 /*    \_| |_/  \_/\_/   \___||_| \__, | \___/ |_|   |_| \__||_| |_||_| |_| |_||___/ 
-/*                                __/ |                                             
-/*                               |___/                                                                                                                                             
+/*                                __/ |
+/*                               |___/                               
 /******************************************************************************************************/
 
 /***********************************
-/* 07_PatternChallenge
+/* 09_PatternChallenge
 /***********************************/
 
 // Library imports
@@ -19,7 +19,7 @@
 using namespace std;
 
 //Prototypes for the algorithms
-void printPattern_07(int);
+void printPattern_09(int);
 
 int main(){
 	// user input
@@ -28,31 +28,39 @@ int main(){
 	cout<<"Enter number of lines."<<endl;
 	cin>>userInput;
 
-	printPattern_07(userInput);
+	printPattern_09(userInput);
 	return 0;
 }
 
 /**********************************************************
-/*                <printPattern_07>
+/*                <printPattern_09>
 /*
-/* @description : Prints integer tables of the respective lines upto 9
+/* @description : Prints a pyramid pattern of digits in increasing and then decreasing order.
 /* @param       : Integer<input>
 /* @return      : None, void
-/* @complexity  : O(n^2)
-/* @explanation : Algorithm that prints tables of integers upto 'input' numbers from 1-9 multiples.
-/*                The outer loop iterates 'input' number of times. The inner loop iterates 'input'
-/*                number of times, outputs i*j covering multiples of i and j in every line.
+/* @complexity  : O(n)
+/* @explanation : Algorithm that first prints an incremental pyramid for the first half of the height,
+/*                then prints the lower half of the height as a decremental mirror pyramid. 
 /* @author      : Pranay Kothapalli @kotAPI
 /**********************************************************/
-void printPattern_07(int input){
+void printPattern_09(int input){
 	// Outer loop loops input number of times
-	
-	// Loop to iterate 'input' times
 	for(int i=1;i<input;i++){
-		// Loop to multiply j with i. Prints tables 1 to input range.
-		for(int j=1;j<input;j++){
-			cout<<i*j<<" ";
+		
+		// check for mid point of the pyramid
+		if(i<=input/2){
+			// for-loop to print the upper half if input <= input/2
+			for(int j=1;j<=i;j++){
+				cout<<j;
+			}
+		}else{ // Enter here if the i value is greater than mid point (input/2) of the pyramid height.
+			// Print the lower half of the pyramid.
+			for(int j=1;j<=input-i;j++){
+				cout<<j;
+			}
 		}
+		
+		// Jump to newline when the inner loops have finished executing.
 		cout<<endl;
 	}
 }
